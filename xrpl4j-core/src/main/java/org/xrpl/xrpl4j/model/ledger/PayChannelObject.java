12841418159394813d7bcb620e35d47236ff6aa7
Jiri Marek <jirka@talos.cz>
2023-09-02 10:24:26 +0200

    default for paychannel balance

--- xrpl4j-core/src/main/java/org/xrpl/xrpl4j/model/ledger/PayChannelObject.java
+++ xrpl4j-core/src/main/java/org/xrpl/xrpl4j/model/ledger/PayChannelObject.java
@@ -9,9 +9,9 @@ package org.xrpl.xrpl4j.model.ledger;
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *      http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -29,6 +29,7 @@ import org.immutables.value.Value;
 import org.xrpl.xrpl4j.model.transactions.Address;
 import org.xrpl.xrpl4j.model.transactions.XrpCurrencyAmount;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
 /**
@@ -52,128 +53,131 @@ import java.util.Optional;
 @JsonDeserialize(as = ImmutablePayChannelObject.class)
 public interface PayChannelObject extends LedgerObject {
 
-  /**
-   * Construct a builder for this class.
-   *
-   * @return An {@link ImmutablePayChannelObject.Builder}.
-   */
-  static ImmutablePayChannelObject.Builder builder() {
-    return ImmutablePayChannelObject.builder();
-  }
+    /**
+     * Construct a builder for this class.
+     *
+     * @return An {@link ImmutablePayChannelObject.Builder}.
+     */
+    static ImmutablePayChannelObject.Builder builder() {
+        return ImmutablePayChannelObject.builder();
+    }
 
-  /**
-   * The type of ledger object, which is always "PayChannel".
-   *
-   * @return Always {@link org.xrpl.xrpl4j.model.ledger.LedgerObject.LedgerEntryType#PAY_CHANNEL}.
-   */
-  @JsonProperty("LedgerEntryType")
-  @Value.Derived
-  default LedgerEntryType ledgerEntryType() {
-    return LedgerEntryType.PAY_CHANNEL;
-  }
+    /**
+     * The type of ledger object, which is always "PayChannel".
+     *
+     * @return Always {@link org.xrpl.xrpl4j.model.ledger.LedgerObject.LedgerEntryType#PAY_CHANNEL}.
+     */
+    @JsonProperty("LedgerEntryType")
+    @Value.Derived
+    default LedgerEntryType ledgerEntryType() {
+        return LedgerEntryType.PAY_CHANNEL;
+    }
 
-  /**
-   * The source {@link Address} that owns this payment channel. This comes from the sending {@link Address} of
-   * the {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel.
-   *
-   * @return The {@link Address} of the source account.
-   */
-  @JsonProperty("Account")
-  Address account();
+    /**
+     * The source {@link Address} that owns this payment channel. This comes from the sending {@link Address} of
+     * the {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel.
+     *
+     * @return The {@link Address} of the source account.
+     */
+    @JsonProperty("Account")
+    Address account();
 
-  /**
-   * The destination {@link Address} for this payment channel. While the payment channel is open, this {@link Address}
-   * is the only one that can receive XRP from the channel. This comes from the
-   * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate#destination()} field of the transaction
-   * that created the channel.
-   *
-   * @return The {@link Address} of the destination account.
-   */
-  @JsonProperty("Destination")
-  Address destination();
+    /**
+     * The destination {@link Address} for this payment channel. While the payment channel is open, this {@link Address}
+     * is the only one that can receive XRP from the channel. This comes from the
+     * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate#destination()} field of the transaction
+     * that created the channel.
+     *
+     * @return The {@link Address} of the destination account.
+     */
+    @JsonProperty("Destination")
+    Address destination();
 
-  /**
-   * Total XRP, in drops, that has been allocated to this channel. This includes XRP that has been paid to the
-   * {@link #destination()} address. This is initially set by the
-   * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel and can
-   * be increased if the source address sends a {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelFund}
-   * transaction.
-   *
-   * @return An {@link XrpCurrencyAmount} representing the total amount allocated to this channel.
-   */
-  @JsonProperty("Amount")
-  XrpCurrencyAmount amount();
+    /**
+     * Total XRP, in drops, that has been allocated to this channel. This includes XRP that has been paid to the
+     * {@link #destination()} address. This is initially set by the
+     * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel and can
+     * be increased if the source address sends a {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelFund}
+     * transaction.
+     *
+     * @return An {@link XrpCurrencyAmount} representing the total amount allocated to this channel.
+     */
+    @JsonProperty("Amount")
+    XrpCurrencyAmount amount();
 
-  /**
-   * Total XRP, in drops, already paid out by the channel. The difference between this value and the
-   * {@link #amount()} field is how much XRP can still be paid to the {@link #destination()} address with
-   * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelClaim} transactions. If the channel closes,
-   * the remaining difference is returned to the {@link #account()} address.
-   *
-   * @return An {@link XrpCurrencyAmount} representing the total amount paid out by this channel.
-   */
-  @JsonProperty("Balance")
-  XrpCurrencyAmount balance();
+    /**
+     * Total XRP, in drops, already paid out by the channel. The difference between this value and the
+     * {@link #amount()} field is how much XRP can still be paid to the {@link #destination()} address with
+     * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelClaim} transactions. If the channel closes,
+     * the remaining difference is returned to the {@link #account()} address.
+     *
+     * @return An {@link XrpCurrencyAmount} representing the total amount paid out by this channel.
+     */
+    @JsonProperty("Balance")
+    @Value.Default
+    default XrpCurrencyAmount balance() {
+        return XrpCurrencyAmount.of(BigInteger.ZERO);
+    }
 
-  /**
-   * Public key, in hexadecimal, of the key pair that can be used to sign claims against this channel.
-   * This can be any valid secp256k1 or Ed25519 public key. This is set by the
-   * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel and
-   * must match the public key used in claims against the channel. The channel {@link #account()} can also send
-   * XRP from this channel to the {@link #destination()} without signed claims.
-   *
-   * @return A {@link String} containing the payment channel's public key.
-   */
-  @JsonProperty("PublicKey")
-  String publicKey();
+    /**
+     * Public key, in hexadecimal, of the key pair that can be used to sign claims against this channel.
+     * This can be any valid secp256k1 or Ed25519 public key. This is set by the
+     * {@link org.xrpl.xrpl4j.model.transactions.PaymentChannelCreate} transaction that created the channel and
+     * must match the public key used in claims against the channel. The channel {@link #account()} can also send
+     * XRP from this channel to the {@link #destination()} without signed claims.
+     *
+     * @return A {@link String} containing the payment channel's public key.
+     */
+    @JsonProperty("PublicKey")
+    String publicKey();
 
-  /**
-   * Number of seconds the source address must wait to close the channel if it still has any XRP in it.
-   * Smaller values mean that the {@link #destination()} has less time to redeem any outstanding claims after the
-   * source address requests to close the channel.
-   *
-   * @return An {@link UnsignedLong} representing the settlement delay.
-   */
-  @JsonProperty("SettleDelay")
-  UnsignedLong settleDelay();
+    /**
+     * Number of seconds the source address must wait to close the channel if it still has any XRP in it.
+     * Smaller values mean that the {@link #destination()} has less time to redeem any outstanding claims after the
+     * source address requests to close the channel.
+     *
+     * @return An {@link UnsignedLong} representing the settlement delay.
+     */
+    @JsonProperty("SettleDelay")
+    UnsignedLong settleDelay();
 
-  /**
-   * The mutable expiration time for this payment channel, in
-   * <a href="https://xrpl.org/basic-data-types.html#specifying-time">seconds since the Ripple Epoch</a>.
-   * The channel is expired if this value is present and smaller than the previous
-   * {@link LedgerHeader#closeTime()} field.
-   *
-   * @return An {@link Optional} of type {@link UnsignedLong} representing the expiration time.
-   */
-  @JsonProperty("Expiration")
-  Optional<UnsignedLong> expiration();
+    /**
+     * The mutable expiration time for this payment channel, in
+     * <a href="https://xrpl.org/basic-data-types.html#specifying-time">seconds since the Ripple Epoch</a>.
+     * The channel is expired if this value is present and smaller than the previous
+     * {@link LedgerHeader#closeTime()} field.
+     *
+     * @return An {@link Optional} of type {@link UnsignedLong} representing the expiration time.
+     */
+    @JsonProperty("Expiration")
+    Optional<UnsignedLong> expiration();
 
-  /**
-   * The immutable expiration time for this payment channel, in
-   * <a href="https://xrpl.org/basic-data-types.html#specifying-time">seconds since the Ripple Epoch</a>.
-   * This channel is expired if this value is present and smaller than the previous {@link LedgerHeader#closeTime()}
-   * field.
-   *
-   * @return An {@link Optional} of type {@link UnsignedLong} representing the cancel after time.
-   */
-  @JsonProperty("CancelAfter")
-  Optional<UnsignedLong> cancelAfter();
+    /**
+     * The immutable expiration time for this payment channel, in
+     * <a href="https://xrpl.org/basic-data-types.html#specifying-time">seconds since the Ripple Epoch</a>.
+     * This channel is expired if this value is present and smaller than the previous {@link LedgerHeader#closeTime()}
+     * field.
+     *
+     * @return An {@link Optional} of type {@link UnsignedLong} representing the cancel after time.
+     */
+    @JsonProperty("CancelAfter")
+    Optional<UnsignedLong> cancelAfter();
 
-  /**
-   * An arbitrary tag to further specify the {@link #account()} for this payment channel, such as a hosted recipient
-   * at the owner's address.
-   *
-   * @return An {@link Optional} of type {@link UnsignedInteger} representing the tag of the source account.
-   */
-  @JsonProperty("SourceTag")
-  Optional<UnsignedInteger> sourceTag();
+    /**
+     * An arbitrary tag to further specify the {@link #account()} for this payment channel, such as a hosted recipient
+     * at the owner's address.
+     *
+     * @return An {@link Optional} of type {@link UnsignedInteger} representing the tag of the source account.
+     */
+    @JsonProperty("SourceTag")
+    Optional<UnsignedInteger> sourceTag();
 
-  /**
-   * An arbitrary tag to further specify the {@link #destination()} for this payment channel, such as a hosted
-   * recipient at the destination address.
-   *
-   * @return An {@link Optional} of type {@link UnsignedInteger} representing the tag of the destination account.
-   */
-  @JsonProperty("DestinationTag")
-  Optional<UnsignedInteger> destinationTag();
+    /**
+     * An arbitrary tag to further specify the {@link #destination()} for this payment channel, such as a hosted
+     * recipient at the destination address.
+     *
+     * @return An {@link Optional} of type {@link UnsignedInteger} representing the tag of the destination account.
+     */
+    @JsonProperty("DestinationTag")
+    Optional<UnsignedInteger> destinationTag();
 }
